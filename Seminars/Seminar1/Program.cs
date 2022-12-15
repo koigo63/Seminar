77cd3195cbc331d3932ec0faafeb04815eae9f30
Igor Kotov <igorkotov@example.com>
2022-12-15 22:51:15 +0400

    1 Seminar

---
+++ Seminars/Seminar1/Program.cs
@@ -0,0 +1,45 @@
+﻿//Console.WriteLine("Введи число");
+//int x = Convert.ToInt32( Console.ReadLine());
+//Console.WriteLine($"Квадрат={x*x}");
+
+//a = 25; b = 5 -> да
+//a = 2 b = 10 -> нет
+//a = 9; b = -3 -> да
+//a = -3 b = 9 -> нет
+//Console.WriteLine("Введи первое число");
+//int a = Convert.ToInt32( Console.ReadLine());
+//Console.WriteLine("Введи второе число");
+//int b = Convert.ToInt32( Console.ReadLine());
+
+
+
+
+
+//int qvad=b*b;
+//if (qvad==a) 
+//{
+// Console.WriteLine($"{a} квадрат {b}");   
+//}
+//else
+//{
+//     Console.WriteLine($"{a} не квадрат {b}");
+//}
+Console.Clear();
+
+ Console.WriteLine("Введи число");
+ int x = Convert.ToInt32( Console.ReadLine());
+ int i=-x;
+ while (i<x+1)
+{
+Console.Write($" { i } ");
+Console.WriteLine("");
+    i=i+1;
+}
+
+//  Console.WriteLine("Введи число");
+//  int q = Convert.ToInt32( Console.ReadLine());
+//  int y=q %10;
+//  Console.WriteLine($"{y}");
+//  //int z=q-y
+// //  Console.WriteLine($"{z}");
+
