f1b6819e9e193750bc332e182f5ee09262d81436
Igor Kotov <igorkotov@example.com>
2023-03-09 14:12:17 +0400

    09-03-23

---
+++ Seminars/Seminar 09-03-23/data.js
@@ -0,0 +1,31 @@
+const dataInfo = `[
+    {
+    "firstName": "Joe2",
+    "lastName": "Jackson",
+    "gender": "male",
+    "age": 28,
+    "address": {
+        "streetAddress": "101",
+        "city": "San Diego",
+        "state": "CA"
+    },
+    "phoneNumbers": [
+        { "type": "home", "number": "7349282382" }
+    ]
+ },
+{
+    "firstName": "Joe1",
+    "lastName": "Jackson",
+    "gender": "male",
+    "age": 28,
+    "address": {
+        "streetAddress": "101",
+        "city": "San Diego",
+        "state": "CA"
+    },
+    "phoneNumbers": [
+        { "type": "home", "number": "7349282382" }
+    ]
+ }
+]
+`
