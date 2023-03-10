7ce9963b6cf9c9b8ebc75f20c85cd11afcbd103c
Igor Kotov <igorkotov@example.com>
2023-03-10 07:39:15 +0400

    Домашка 09-03-23

---
+++ Homeworks/Home 09-03-23/data.js
@@ -0,0 +1,22 @@
+const dataInfo = `
+[
+    {
+      "age": 62,
+      "count": 298219,
+      "name": "michael",
+      "photo":"https://avatars.mds.yandex.net/i?id=13b00c3131922d28102ce6cd5e9a35c0b9ec4081-6380060-images-thumbs&n=13"
+    },
+    {
+      "age": 44,
+      "count": 53379,
+      "name": "matthew",
+      "photo":"https://artpakdv.ru/image/cache/catalog/import_files/9f/9fa2a7c73b8911e8810dd017c296eff9_9fa2a7c83b8911e8810dd017c296eff9-800x800.png"
+    },
+    {
+      "age": 68,
+      "count": 40935,
+      "name": "jane",
+      "photo":"https://strateg.ua/image/cache/catalog/1Cproducts/PL-00034289_00-1000x1000.jpg"
+    }
+  ]
+`
