421877fc79077687fd4e613faaa1b9f37cfea837
Igor Kotov <igorkotov@example.com>
2023-04-16 15:50:52 +0400

    16-04

--- JAVA/proekt/src/App.java
+++ JAVA/proekt/src/App.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         // System.out.println("Hello, World!");
-        int x = 3;
-        String name = "Кинжал";
-        x = x * 17;
+        // int x = 3;
+        // String name = "Кинжал";
+        // x = x * 17;
         // System.out.println("x равен" + x);
         // double d = Math.random();
         // while (x > 12) {
@@ -18,6 +20,27 @@ public class App {
         // } else {
         // System.out.println("x не равен 10");
         // }
-        System.out.println(name.equals("Кинжал"));
+        // System.out.println(name.equals("Кинжал"));
+        // // String s = new String("set");
+        Scanner sd = new Scanner(System.in);
+        // String string = sd.nextLine(); // int nextInt()
+        // // System.out.println(string);
+        // int value;
+        // do {
+        // System.out.println("Введите 5");
+        // value = sd.nextInt();
+        // } while (value != 5);
+        // System.out.println("Введили 5");
+        System.out.println("Введите возраст");
+        int age = sd.nextInt();
+        switch (age) {
+            case 0:
+                System.out.println("0");
+                break;
+
+            default:
+                break;
+        }
+
     }
 }
